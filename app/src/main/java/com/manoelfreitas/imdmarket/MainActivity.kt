cd8373326b52846e7fd12421de75c85faa8d8ae9
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-11-28 23:24:24 -0300

    feat: create SplashScreen and routes

--- app/src/main/java/com/manoelfreitas/imdmarket/MainActivity.kt
+++ app/src/main/java/com/manoelfreitas/imdmarket/MainActivity.kt
@@ -3,45 +3,78 @@ package com.manoelfreitas.imdmarket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.manoelfreitas.imdmarket.ui.theme.IMDMarketTheme
+import androidx.compose.runtime.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+import com.manoelfreitas.imdmarket.ui.screen.splash.SplashScreen
+import com.manoelfreitas.imdmarket.ui.screen.auth.login.LoginScreen
+import com.manoelfreitas.imdmarket.ui.screen.main.menu.MenuScreen
+import com.manoelfreitas.imdmarket.ui.screen.main.item.additem.AddItemScreen
+import com.manoelfreitas.imdmarket.ui.screen.main.item.edititem.EditItemScreen
+import com.manoelfreitas.imdmarket.ui.screen.main.item.deleteitem.DeleteItemScreen
+import com.manoelfreitas.imdmarket.ui.screen.main.item.listitems.ListItemScreen
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
+
         setContent {
-            IMDMarketTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigation() {
+    val navController = rememberNavController()
+    var showSplashScreen by remember { mutableStateOf(true) }
+
+    LaunchedEffect(key1 = true) {
+        delay(1000)
+        showSplashScreen = false
+    }
+
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") {
+            SplashScreen(navController)
+        }
+        composable("login"){
+            LoginScreen(navController)
+        }
+        composable("menu"){
+            MenuScreen(navController)
+        }
+        composable("additem"){
+            AddItemScreen(navController)
+        }
+        composable("edititem"){
+            EditItemScreen(navController)
+        }
+        composable("deleteitem"){
+            DeleteItemScreen(navController)
+        }
+        composable("listitems"){
+            ListItemScreen(navController)
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IMDMarketTheme {
-        Greeting("Android")
+fun NavController.navigateToLogin(){
+    navigate("login"){
+        popUpTo("splash"){inclusive = true}
+    }
+}
+
+fun NavController.navigateToMenu(){
+    navigate("menu"){
+        popUpTo("login"){inclusive = true}
     }
 }
---
+++ app/src/main/java/com/manoelfreitas/imdmarket/ui/screen/splash/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.manoelfreitas.imdmarket.ui.screen.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.manoelfreitas.imdmarket.R
+import com.manoelfreitas.imdmarket.navigateToLogin
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ){
+        Image(painter = painterResource(id = R.drawable.baseline_storefront_24),
+            contentDescription = "Logo App",
+            modifier = Modifier.size(256.dp))
+    }
+    
+    LaunchedEffect(key1 = true){
+        delay(1000)
+        navController.navigateToLogin()
+    }
+}

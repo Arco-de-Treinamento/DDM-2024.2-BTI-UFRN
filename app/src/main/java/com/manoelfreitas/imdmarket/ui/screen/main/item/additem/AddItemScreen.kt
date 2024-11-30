abee07b2a0db66bcc20509f8590e3ab03ffb833d
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-11-29 21:56:34 -0300

    feat: criando tela de cadastro de item

--- app/src/main/java/com/manoelfreitas/imdmarket/ui/screen/main/item/additem/AddItemScreen.kt
+++ app/src/main/java/com/manoelfreitas/imdmarket/ui/screen/main/item/additem/AddItemScreen.kt
@@ -1,67 +1,102 @@
 package com.manoelfreitas.imdmarket.ui.screen.main.item.additem
 
-
-import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.manoelfreitas.imdmarket.navigateToMenu
-import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddItemScreen(navController: NavController){
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    val coroutineScope = rememberCoroutineScope()
-    val context = LocalContext.current
+    var productCode by remember {(mutableStateOf(""))}
+    var productName by remember {(mutableStateOf(""))}
+    var productDescription by remember {(mutableStateOf(""))}
+    var productQuantity by remember {(mutableStateOf(""))}
 
-    Column (modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center){
+    Scaffold (
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                        Text(text = "IMD Market")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Voltar")
+                    }
+                },
 
-        OutlinedTextField(value = username,
-            onValueChange = {username = it },
-            label = {Text("Usuário")},
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = password,
-            onValueChange = {password = it },
-            label = {Text("Senha") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { coroutineScope.launch { checkLogin(
-            context = context,
-            username = username,
-            password = password,
-            navController = navController)}}) {
-            Text("Login")
+                colors = TopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    scrolledContainerColor = MaterialTheme.colorScheme.primary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
+            )
+        }
+        ) { innerPadding ->
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+            Row (modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center){
+                Text(text = "CADASTRAR PRODUTO", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+            }
+            Spacer(modifier = Modifier.height(28.dp))
+            OutlinedTextField(value = productCode,
+                onValueChange = {productCode = it },
+                label = {Text("Código do produto:")},
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(value = productName,
+                onValueChange = {productName = it },
+                label = {Text("Nome do Produto:") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(value = productDescription,
+                onValueChange = {productDescription = it },
+                label = {Text("Descrição do Produto:") },
+                modifier = Modifier.fillMaxWidth(),
+                maxLines = 3
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(value = productQuantity,
+                onValueChange = {productQuantity = it },
+                label = {Text("Quantidade em Estoque:") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(28.dp))
+            Row (modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween) {
+                Button(
+                    onClick = { }) {
+                    Text("Cadastrar")
+                }
+                Button(onClick = {
+                    productCode = ""
+                    productName = ""
+                    productDescription = ""
+                    productQuantity = ""
+                },
+                ) {
+                    Text("Limpar")
+                }
+            }
         }
     }
 }
 
-fun checkLogin(context: Context, username: String, password: String, navController: NavController): () -> Unit {
-    if (username == "admin" && password == "admin") {
-        // Navigate to MenuScreen
-        navController.navigateToMenu()
-    } else {
-        Toast.makeText(context, "Falha ao realizar o login!", Toast.LENGTH_SHORT).show()
-    }
-
-    return {}
-}

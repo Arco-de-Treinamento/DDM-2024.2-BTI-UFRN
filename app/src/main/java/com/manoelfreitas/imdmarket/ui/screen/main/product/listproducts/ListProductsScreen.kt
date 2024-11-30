24b33297e24dac67ec6dfcb9f70065685c5dcca9
Manoel Freitas <josmanoel.freitas@gmail.com>
2024-11-30 18:21:54 -0300

    feat: create a listview items

--- app/src/main/java/com/manoelfreitas/imdmarket/ui/screen/main/product/listproducts/ListProductsScreen.kt
+++ app/src/main/java/com/manoelfreitas/imdmarket/ui/screen/main/product/listproducts/ListProductsScreen.kt
@@ -1,6 +1,7 @@
 package com.manoelfreitas.imdmarket.ui.screen.main.product.listproducts
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
@@ -15,15 +16,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.manoelfreitas.imdmarket.R
 import com.manoelfreitas.imdmarket.ui.screen.main.product.Product
 import com.manoelfreitas.imdmarket.ui.screen.main.product.ProductViewModel
 
@@ -63,14 +69,7 @@ fun ListProductScreen(navController: NavController){
             .padding(16.dp),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center){
-            Row (modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center){
-                Text(text = "LISTA DE PRODUTOS", fontWeight = FontWeight.Bold, fontSize = 24.sp)
-            }
-            Spacer(modifier = Modifier.height(48.dp))
-            LazyColumn(
-                contentPadding = PaddingValues(8.dp),
-            ){
+            LazyColumn(){
                 items(_products.size){
                     index -> ProductListItem(product = _products[index])
                 }
@@ -88,15 +87,24 @@ fun ProductListItem(product: Product) {
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Column(
+        Row (
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(text = "Código: ${product.productCode}")
-            Text(text = "Nome: ${product.productName}")
-            Text(text = "Descrição: ${product.productDescription}")
-            Text(text = "Quantidade: ${product.productQuantity}")
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(painter = painterResource(id = R.drawable.baseline_storefront_24),
+                contentDescription = "Logo App",
+                modifier = Modifier.size(48.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = product.productName, fontWeight = FontWeight.Bold, fontSize = 14.sp)
+                Text(text = product.productDescription, fontSize = 12.sp, color = Color.Gray)
+            }
         }
     }
 
